e2ef8c70e3f1c86fc05070ca5b6db2476f39ef71
Braydon Whitmarsh <braydonwhitmarsh@me.com>
2017-08-15 19:14:51 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,47 @@
+
+function forLoop(array)
+{
+  for (let i = 0; i < 25; i++)
+    {
+      array.push('I am ${i} strange loop')
+    }
+    return array
+}
+
+
+function whileLoop(n)
+{
+  while (0 < n) {
+    console.log(--n);
+  }
+
+  return ("done")
+}
+
+function isItTrue()
+{
+  return true
+}
+
+function doWhileLoop(array) {
+
+if(length.array === 0)
+{
+  isItTrue() === false
+  return array
+}
+if(length.array > 6)
+{
+  array.pop([length.array])
+}
+else
+{
+  isItTrue() === true
+  return array
+}
+
+do {
+  return array
+} while (isItTrue());
+
+}
